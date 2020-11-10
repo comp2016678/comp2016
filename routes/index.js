8bfb893c7b798e880c864c362707b9682c5da280
comp2106 <tm_1001@outlook.com>
2020-11-10 14:02:12 +0500

    Initial Development

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Comp2068 - Homepage' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Comp2068 - About' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Comp2068 - Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Comp2068 - Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Comp2068 - Contact' });
+});
 module.exports = router;
